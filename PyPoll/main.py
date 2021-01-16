6c52799f3dfa0feab08090c0bccd46728bd65bd1
code_red <72528267+Corters22@users.noreply.github.com>
2021-01-15 22:06:37 -0600

    final code with print to text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,18 +24,21 @@ with open(csvpath) as csvfile:
                 num_of_votes += 1
         total_cand_vote.append(num_of_votes)
 
-
-    final_vote = zip(unique_cand, total_cand_vote)
-    for row in final_vote:
-        print(row[0] + ": " + f'{((row[1]/total_votes) *100):.3f}' + "% " + "(" + str(row[1]) + ")")
-        if row[1] == max(total_cand_vote):
-            winner = row[0]        
+vote_results = []
+final_vote = zip(unique_cand, total_cand_vote)
+for row in final_vote:
+    vote_results.append(row[0] + ": " + f'{((row[1]/total_votes) *100):.3f}' + "% " + "(" + str(row[1]) + ")")
+    if row[1] == max(total_cand_vote):
+        winner = row[0]        
 
 print("Election Results")
 print("-" * 20)
 print("Total Votes: " + str(total_votes))
 print("-" * 20)
-
+print(vote_results[0])
+print(vote_results[1])
+print(vote_results[2])
+print(vote_results[3])
 print("-" * 20)
 print("Winner: " + winner)
 print("-" * 20)
@@ -51,7 +54,10 @@ with open(output_path, 'w', newline='') as textfile:
     print("-" * 20, file=textfile)
     print("Total Votes: " + str(total_votes), file=textfile)
     print("-" * 20, file=textfile)
-    print(vote_results(), file=textfile)
+    print(vote_results[0], file=textfile)
+    print(vote_results[1], file=textfile)
+    print(vote_results[2], file=textfile)
+    print(vote_results[3], file=textfile)
     print("-" * 20, file=textfile)
     print("Winner: " + winner, file=textfile)
     print("-" * 20, file=textfile)
