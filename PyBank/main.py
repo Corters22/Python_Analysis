54622c9e554c15a45b7ece5f26fef74acbfc225a
code_red <72528267+Corters22@users.noreply.github.com>
2021-01-15 18:38:36 -0600

    added comment lines

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,13 @@
 import csv
 import os
 
+#joining csv file with code
 csvpath = os.path.join("Resources", 'budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     
+    #calculations and making new lists
     period_change_list = []
     date = []
     prof_loss = []
@@ -13,15 +15,21 @@ with open(csvpath) as csvfile:
     total =  0
     profit_losses = 0
     for row in csvreader:
+        #calculating count of rows
         row_count = row_count + 1
+        #calculation total profits/losses
         total = total + int(row[1])
+        #calculating changes between two date periods
         profit_losses_2 = int(row[1])
         period_change = profit_losses_2 - profit_losses
         period_change_list.append(period_change)
+        #making new lists
         date.append(row[0])
         prof_loss.append(row[1]) 
+        #resetting profit loss value
         profit_losses = int(row[1])  
 
+#zipping lists to get max/min and coordinating dates
 zippy = zip(date, prof_loss, period_change_list) 
 for row in zippy:
     if row[2] == max(period_change_list):
@@ -29,16 +37,16 @@ for row in zippy:
     elif row[2] == min(period_change_list):
         great_dec_per = row[0]
             
-    
+#calculating the total of the profit/loss changes    
 total_change = 0
 del period_change_list[0]
 for value in period_change_list:
     total_change = total_change + value
-    
+#calculating avg of profit/loss changes    
 avg_change = total_change/ (len(period_change_list))
 
             
-
+#print to terminal
 print("Financial Analysis")
 print("--------------------------")
 print("Total Months: " + str(row_count))
@@ -47,8 +55,10 @@ print("Average Change: $" + f'{avg_change:.2f}')
 print("Greatest Increase in Profits: " + great_inc_per + ' ($' + str(max(period_change_list)) + ')')
 print("Greatest Decrease in Profits: " + great_dec_per + ' ($' + str(min(period_change_list)) + ')')
 
+#open new text file
 output_path = os.path.join("Analysis", "results.txt")
 with open(output_path, 'w', newline='') as textfile:
+    #print to text file
     print("Financial Analysis", file=textfile)
     print("--------------------------", file=textfile)
     print("Total Months: " + str(row_count), file=textfile)
